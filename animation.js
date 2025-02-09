3664a1fc538180d0f98acfcea78d52364928a3a8
vinkel <vincent.benoot@gmail.com>
2025-02-09 18:57:29 +0100

    Something wrong with animation?

--- animation.js
+++ animation.js
@@ -1,7 +1,11 @@
+// INIT -------------------------
+const canvas = document.getElementById("canvas")
+const context = canvas.getContext("2d")
+let raf
 // Constants --------------------
 const boidsAmount = 100
 // It seems the most fun to let everything start at the same speed.
-const startVelocity = 5.0
+const startVelocity = 4
 const boids = createBoids()
 
 const shyness = 0.05
@@ -14,47 +18,44 @@ const matchingfactor = 0.05
 const maxspeed = 6
 const minspeed = 3
 // Main script -----------------
-const canvas = document.getElementById("canvas")
-const context = canvas.getContext("2d")
-let raf
-
-const ball = {
-    x: 100,
-    y: 100,
-    vx: 3,
-    vy: 2,
-    color: "blue",
-    draw() {
-        context.beginPath();
-        context.arc(this.x, this.y, 2, 0, Math.PI * 2, true);
-        context.closePath();
-        context.fillStyle = this.color;
-        context.fill();
-    }
-}
 
 function draw() {
-    console.log(canvas.height)
     context.clearRect(0, 0, canvas.width, canvas.height)
-    console.log("updating")
-    ball.x = (ball.x + ball.vx) % canvas.width
-    ball.y = (ball.y + ball.vy) % canvas.height
-    console.log(`ball.x = ${ball.x}, ball.y = ${ball.y}`)
-    ball.draw()
+    boids.forEach(drawBoid)
+    update()
     raf = window.requestAnimationFrame(draw)
 }
 
-canvas.addEventListener("mouseover", (e) => {
+function drawBoid(boid) {
+    context.beginPath();
+    context.arc(boid.x, boid.y, 2, 0, Math.PI * 2, true);
+    context.closePath();
+    context.fillStyle = "teal";
+    context.fill();
+}
+
+function update() {
+    updateSeperation()
+    updateCohesion()
+    updateAlignement()
+
+    updateSpeeds()
+
+    boids.forEach(b => {
+        b.x = (b.x + b.vx) % canvas.width
+        b.y = (b.y + b.vy) % canvas.height
+        console.log(b)
+    })
+
+}
+
+canvas.addEventListener("load", (e) => {
     raf = window.requestAnimationFrame(draw);
 });
 
-canvas.addEventListener("mouseout", (e) => {
-    window.cancelAnimationFrame(raf);
-});
+draw();
 
-ball.draw();
-
-// BOIDS
+// BOIDS ------------------------
 function createBoids() {
     const result = []
     for (let i = 0; i < boidsAmount; i++) {
@@ -62,7 +63,7 @@ function createBoids() {
         const y = Math.random() * canvas.height
 
         const vx = Math.random() * startVelocity;
-        const vy = Math.sqrt(vx ** 2 - startVelocity ** 2)
+        const vy = Math.sqrt(Math.abs((vx ** 2) - (startVelocity ** 2)))
 
         result.push({
             x: x,
@@ -130,6 +131,25 @@ function updateCohesion() {
         boid1.vy = (yposAvg - boid1.y) * centeringfactor
     })
 }
+
+/**
+* Keeps the speeds within the boundaries
+*/
+function updateSpeeds() {
+    boids.forEach(boid => {
+        let speed = Math.sqrt(boid.vx ** 2 + boid.vy ** 2)
+        if (speed > maxspeed) {
+            boid.vx = (boid.vx / speed) * maxspeed
+            boid.vy = (boid.vy / speed) * maxspeed
+        }
+        if (speed < minspeed) {
+            boid.vx = (boid.vx / speed) * minspeed
+            boid.vy = (boid.vy / speed) * minspeed
+        }
+
+    })
+}
+
 function seeEachother(boid1, boid2) {
     return distance(boid1.x, boid1.y, boid2.x, boid2.y) < visualRange
 }
